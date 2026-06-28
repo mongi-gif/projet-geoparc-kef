14dfa36d3942915d94d37a5ee3420b0e7fcd4938
mongi-gif <55959817+mongi-gif@users.noreply.github.com>
2026-06-28 12:33:57 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Création de la carte
+
+const map = L.map('map').setView([36.18,8.71],9);
+
+// Fond OpenStreetMap
+
+L.tileLayer(
+
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+
+{
+
+attribution:'© OpenStreetMap'
+
+}
+
+).addTo(map);
+
+
+// Marqueur provisoire
+
+L.marker([36.18,8.71])
+
+.addTo(map)
+
+.bindPopup("<b>Le Kef</b><br>Projet Géoparc")
+
+.openPopup();
